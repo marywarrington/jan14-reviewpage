efd8231ded02a225d15a59e92735f43b63821e81
Anna Lawley and Mary Warrington <marywarrington@gmail.com>
2016-01-14 11:14:36 -0800

    fills out branching page

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,19 @@ $(document).ready(function(){
   $("#attributes-button").click(function() {
     $("body").toggleClass("red-background")
   });
+
+  $("#branching-button").click(function() {
+    var color = prompt("Type either 'green' or 'red'!");
+      if (color === "green") {
+        $("body").removeClass();
+        $("body").addClass("green-background")
+      } else if (color === "red") {
+        $("body").removeClass();
+        $("body").addClass("red-background")
+      } else {
+        alert("You didn't choose a color! Try again!")
+      }
+  });
+
+
 });
